5b8c06a5578bb3066d67461abdc874cd159f174b
Ansoumane Kande <kande.ans@outlook.com>
2018-02-07 20:40:21 -0500

    Updated SearchBar Input

--- src/Containers/searchBar.js
+++ src/Containers/searchBar.js
@@ -3,13 +3,14 @@ import React,{Component} from 'react'
 export default class SearchBar extends Component {
   constructor(props){
   	super(props);
-  	this.state = {term: ''};
-  }
+  	this.state = {term: ''}
 
+    this.onInputChange= this.onInputChange.bind(this)
+  }
   onInputChange(event) {
+    this.setState({term: event.target.value})
     console.log(event.target.value)
   }
-
     render() {
         return (
           <form className="input-group">
